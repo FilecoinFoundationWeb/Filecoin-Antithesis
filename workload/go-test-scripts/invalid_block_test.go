f8ae48eab8cc58a8083ed56f58bf6834c19be013
Ubuntu <ubuntu@ip-172-31-0-150.ec2.internal>
2025-01-28 07:05:17 +0000

    removing delete for today

---
+++ workload/go-test-scripts/invalid_block_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/antithesishq/antithesis-sdk-go/assert"
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/ipfs/go-cid"
+)
+
+func mockTipSet(t *testing.T) *types.TipSet {
+	minerAct, err := address.NewIDAddress(0)
+	assert.Always(err == nil, "Loading the resources config", map[string]interface{}{"error": err})
+	c, err := cid.Decode("QmbFMke1KXqnYyBBWxB74N4c5SBnJMVAiMNRcGu6x1AwQH")
+	assert.Always(err == nil, "Loading the resources config", map[string]interface{}{"error": err})
+
+	blks := []*types.BlockHeader{
+		{
+			Miner:                 minerAct,
+			Height:                abi.ChainEpoch(1),
+			ParentStateRoot:       c,
+			ParentMessageReceipts: c,
+			Messages:              c,
+			Ticket:                &types.Ticket{VRFProof: []byte{}},
+		},
+		{
+			Miner:                 minerAct,
+			Height:                abi.ChainEpoch(1),
+			ParentStateRoot:       c,
+			ParentMessageReceipts: c,
+			Messages:              c,
+			Ticket:                &types.Ticket{VRFProof: []byte{}},
+		},
+	}
+	ts, err := types.NewTipSet(blks)
+	fmt.Println(ts)
+	assert.Always(err == nil, "Loading the resources config", map[string]interface{}{"error": err})
+	return ts
+}
