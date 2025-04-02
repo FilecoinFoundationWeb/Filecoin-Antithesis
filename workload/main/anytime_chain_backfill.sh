28037605c16708278f5898ec4560617cbbf2bc31
Parth Shah <parthhshah171@gmail.com>
2025-04-02 17:55:36 +0000

    renaming-to-right-name

--- workload/main/anytime_chain_backfill.sh
+++ workload/main/anytime_chain_backfill.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Running Test: Chain Backfill"
-go test -v /opt/antithesis/go-test-scripts/chain_backfill_test.go 
+go test -v /opt/antithesis/go-test-scripts/index_chain_backfill_test.go 
