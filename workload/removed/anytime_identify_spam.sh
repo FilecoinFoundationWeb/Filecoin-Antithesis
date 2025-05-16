2781106011fd9e53f22a6be3de691e0b2a7e4305
Parth Shah <parthhshah171@gmail.com>
2025-05-16 14:33:19 +0000

    removing stale test

--- workload/removed/anytime_identify_spam.sh
+++ workload/removed/anytime_identify_spam.sh
@@ -9,7 +9,7 @@ LOTUS_1_TARGET=$(cat "/root/devgen/lotus-1/lotus-1-ipv4addr")
 LOTUS_2_TARGET=$(cat "/root/devgen/lotus-2/lotus-2-ipv4addr")
 
 # Randomly select a Lotus target from the available options.
-random_targets=("LOTUS_1_TARGET" "LOTUS_2_TARGET")
+random_targets=("LOTUS_1_TARGET")
 selected_target=${random_targets[$((RANDOM % ${#random_targets[@]}))]}
 export LOTUS_TARGET=${!selected_target}
 
