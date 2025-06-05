7066d7d96cbf5bbd8fee468eaf8b7f4ff0c17226
Marcus Hodgson <marcus.hodgson@antithesis.com>
2025-06-05 17:21:25 -0400

    Adding top 20 rpc endpoints to lotus-bench script

--- lotus/parallel_lotus_bench.sh
+++ lotus/parallel_lotus_bench.sh
@@ -1,30 +1,47 @@
 #!/usr/bin/env bash
 
-# This script calls the lotus-bench binary from lotus 1
+method_list=(
+    eth_call
+    Filecoin.StateMinerInfo
+    Filecoin.ChainHead
+    eth_getBalance
+    eth_getBlockByNumber
+    eth_blockNumber
+    eth_getLogs
+    eth_getTransactionReceipt
+    eth_getBlockByHash
+    Filecoin.ChainGetTipSetByHeight
+    Filecoin.WalletBalance
+    Filecoin.StateMinerPartitions
+    Filecoin.StateReadState
+    eth_getTransactionByHash
+    eth_getBlockReceipts
+    Filecoin.StateLookupID
+    eth_feeHistory
+    Filecoin.ChainGetParentReceipts
+    Filecoin.ChainGetParentMessages
+    Filecoin.ChainGetTipSet
+)
 
+endpoint_list=(
+    http://lotus-1:1234/rpc/v1
+    http://lotus-2:1235/rpc/v1
+)
 
-odn=$(od -An -N1 -d /dev/urandom)
+get_random_index() {
+    local array_length=$1
+    local rand
+    rand=$(od -An -N2 -tu2 /dev/urandom)
+    rand=${rand//[[:space:]]/}
+    echo $((rand % array_length))
+}
 
-mapped=$(( (odn % 4) + 1))
+method_index=$(get_random_index ${#method_list[@]})
+method=${method_list[$method_index]}
 
-case "$mapped" in
-    1) 
-        echo "Running benchmark test for lotus-1 with method eth_chainId"
-        ./lotus-bench rpc --method='eth_chainId' --endpoint http://lotus-1:1234/rpc/v1
-        ;;
-    2)
-        echo "Running benchmark test for lotus-1 with method Filecoin.F3GetManifest"
-        ./lotus-bench rpc --method='Filecoin.F3GetManifest' --endpoint http://lotus-1:1234/rpc/v1
-        ;;
-    3)
-        echo "Running benchmark test for lotus-2 with method eth_chainId"
-        ./lotus-bench rpc --method='eth_chainId' --endpoint http://lotus-2:1235/rpc/v1
-        ;;
-    4)
-        echo "Running benchmark test for lotus-2 with method Filecoin.F3GetManifest"
-        ./lotus-bench rpc --method='Filecoin.F3GetManifest' --endpoint http://lotus-2:1235/rpc/v1
-        ;;
-    *)
-        echo "This should never happen"
-        ;;
-esac
+endpoint_index=$(get_random_index ${#endpoint_list[@]})
+endpoint=${endpoint_list[$endpoint_index]}
+
+echo "Running benchmark test for $endpoint for method $method"
+
+./lotus-bench rpc --method="$method" --endpoint="$endpoint"
