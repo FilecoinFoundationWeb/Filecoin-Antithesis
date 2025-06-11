c09eaa89611c3528a186c054c7117486ce0ff8f2
Richard <richard.yao@antithesis.com>
2025-06-11 14:12:05 -0400

    Update: assertions must be on one line using fallback sdk

--- workload/main/eventually_forest_healthy.sh
+++ workload/main/eventually_forest_healthy.sh
@@ -9,31 +9,16 @@ URL="http://${FOREST_IP}:${PORT}${ENDPOINT}"
 output_path="${ANTITHESIS_OUTPUT_DIR}/sdk.jsonl"
 
 # initial SDK assertion (assume healthy)
-echo '{"antithesis_assert": {
-    "hit": false, 
-    "must_hit": true, 
-    "assert_type": "always", 
-    "display_type": "Always", 
-    "message": "Forest node stays healthy", 
-    "condition": false, 
-    "id": "Forest node stays healthy", 
-    "location": {}, 
-    "details": null
-    }}' >> "$output_path"
+echo '{"antithesis_assert":{"hit":false,"must_hit":true,"assert_type":"always","display_type":"Always","message":"Forest node stays healthy","condition":false,"id":"Forest node stays healthy","location":{},"details":null}}' >> "$output_path"
+
 
 # initial SDK assertion (assume reachable)
-echo '{"antithesis_assert": {
-    "hit": false, 
-    "must_hit": true, 
-    "assert_type": "always", 
-    "display_type": "Always", 
-    "message": "Forest node stays reachable", 
-    "condition": false, 
-    "id": "Forest node stays reachable", 
-    "location": {}, 
-    "details": null
-    }}' >> "$output_path"
+echo '{"antithesis_assert":{"hit":false,"must_hit":true,"assert_type":"always","display_type":"Always","message":"Forest node stays reachable","condition":false,"id":"Forest node stays reachable","location":{},"details":null}}' >> "$output_path"
 
+
+# ensure no faults when we do the healthcheck
+sleep 20
+# curl for healthcheck
 RESPONSE=$(curl --silent "$URL")
 
 #echo "Response: $RESPONSE"
@@ -45,34 +30,14 @@ if [[ -z "$RESPONSE" ]]; then
   echo "Forest node is unreachable: $RESPONSE"
 
   # Log assertion failure with details
-  echo '{"antithesis_assert": {
-    "hit": true,
-    "must_hit": true,
-    "assert_type": "always",
-    "display_type": "Always",
-    "message": "Forest node stays reachable",
-    "condition": false,
-    "id": "Forest node stays reachable",
-    "location": {},
-    "details": null
-  }}' >> "$output_path"
+  echo '{"antithesis_assert":{"hit":true,"must_hit":true,"assert_type":"always","display_type":"Always","message":"Forest node stays reachable","condition":false,"id":"Forest node stays reachable","location":{},"details":null}}' >> "$output_path"
 
   exit 1
 elif echo "$RESPONSE" | grep -q '\[!\]'; then
   echo "Forest node is unhealthy: $RESPONSE"
 
   # Log assertion failure with details
-  echo '{"antithesis_assert": {
-    "hit": true,
-    "must_hit": true,
-    "assert_type": "always",
-    "display_type": "Always",
-    "message": "Forest node stays healthy",
-    "condition": false,
-    "id": "Forest node stays healthy",
-    "location": {},
-    "details": null
-  }}' >> "$output_path"
+  echo '{"antithesis_assert":{"hit":true,"must_hit":true,"assert_type":"always","display_type":"Always","message":"Forest node stays healthy","condition":false,"id":"Forest node stays healthy","location":{},"details":null}}' >> "$output_path"
 
   exit 1
 else
