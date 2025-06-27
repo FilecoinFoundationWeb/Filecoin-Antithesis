0a5209b00515bd19e1c03c90f94a63d619b20864
Richard <richard.yao@antithesis.com>
2025-06-27 16:27:59 -0400

    Update: actually send the rpc calls

--- forest/forest_mpool_select_killer.sh
+++ forest/forest_mpool_select_killer.sh
@@ -6,13 +6,28 @@ echo "Running regression test for forest MessagePool panic"
 
 export TOKEN=$(cat ${FOREST_DATA_DIR}/jwt)
 export FULLNODE_API_INFO=$TOKEN:/ip4/${FOREST_IP}/tcp/${FOREST_RPC_PORT}/http
+export FOREST_RPC_URL=${FOREST_IP}:${FOREST_RPC_PORT} 
 echo "FULLNODE_API_INFO: $FULLNODE_API_INFO"
 
-forest-cli chain head --format json -n 100 | jq -c '.[] | {
-  jsonrpc: "2.0",
-  method: "Filecoin.MpoolSelect",
-  params: [(.cids | map({"/": .})), 0.8],
-  id: 1
-}'
+# Get the last 100 tipsets and send MpoolSelect requests for each
+forest-cli chain head --format json -n 100 | \
+jq -c '.[] | { cids: .cids }' | \
+while read -r line; do
+  CIDS=$(echo "$line" | jq -c '[.cids[] | {"\/": .}]')
+  JSON=$(jq -n --argjson cids "$CIDS" --argjson id 1 '{
+    jsonrpc: "2.0",
+    method: "Filecoin.MpoolSelect",
+    params: [$cids, 0.8],
+    id: $id
+  }')
+
+  echo "Sending request: $JSON"
+
+  curl -sS -X POST "$FOREST_RPC_URL" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $TOKEN" \
+    -d "$JSON" || echo "Request failed"
+
+done
 
 echo "Finished running regression test for forest MessagePool panic"
