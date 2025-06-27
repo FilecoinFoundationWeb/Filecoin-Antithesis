074439dfdf23983d176a8b71b670aa6bf7e7d104
Richard <richard.yao@antithesis.com>
2025-06-27 15:45:49 -0400

    Feat: initial forest regression test stuff

--- forest/forest_mpool_select_killer.sh
+++ forest/forest_mpool_select_killer.sh
@@ -4,6 +4,10 @@
 
 echo "Running regression test for forest MessagePool panic"
 
+export TOKEN=$(cat ${FOREST_DATA_DIR}/jwt)
+export FULLNODE_API_INFO=$TOKEN:/ip4/${FOREST_IP}/tcp/${FOREST_RPC_PORT}/http
+echo "FULLNODE_API_INFO: $FULLNODE_API_INFO"
+
 forest-cli chain head --format json -n 100 | jq -c '.[] | {
   jsonrpc: "2.0",
   method: "Filecoin.MpoolSelect",
