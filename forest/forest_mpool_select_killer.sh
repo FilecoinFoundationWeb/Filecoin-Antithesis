e9d36280fe0543bfaf78be8c4f35f5c9bdeb58f5
Richard <richard.yao@antithesis.com>
2025-07-01 17:40:07 -0400

    update: msging for script

--- forest/forest_mpool_select_killer.sh
+++ forest/forest_mpool_select_killer.sh
@@ -3,25 +3,27 @@
 # Fetch the last 100 tipsets from forest-cli and construct JSON-RPC requests for MpoolSelect method
 
 echo "Running regression test for forest MessagePool panic"
+# https://github.com/ChainSafe/forest/issues/4490
 
 export TOKEN=$(cat ${FOREST_DATA_DIR}/jwt)
 export FULLNODE_API_INFO=$TOKEN:/ip4/${FOREST_IP}/tcp/${FOREST_RPC_PORT}/http
 export FOREST_RPC_URL="http://${FOREST_IP}:${FOREST_RPC_PORT}/rpc/v0"
-echo "FULLNODE_API_INFO: $FULLNODE_API_INFO"
-echo "FOREST_RPC_URL: $FOREST_RPC_URL"
+#echo "FULLNODE_API_INFO: $FULLNODE_API_INFO"
+#echo "FOREST_RPC_URL: $FOREST_RPC_URL"
 
 # Check if we have 100 tipsets
-HEIGHT=$(forest-cli chain head --format json -n 100 | jq 'length')
+TIPSETS=$(forest-cli chain head --format json -n 100)
+HEIGHT=$($TIPSETS | jq 'length')
 echo "Current height: $HEIGHT"
 if [ "$HEIGHT" -lt 100 ]; then
-  echo "Chain head is not at 100 yet!. Exiting script.."
+  echo "Chain head is not at 100 yet! Exiting script.."
   exit 1
 else
-  echo "Chain head is at 100!. Running script.."
+  echo "Chain head is at 100! Running script.."
 fi
 
 # Get the last 100 tipsets and send MpoolSelect requests for each
-forest-cli chain head --format json -n 100 | \
+$TIPSETS \
 jq -c '.[] | { cids: .cids }' | \
 while read -r line; do
   CIDS=$(echo "$line" | jq -c '[.cids[] | {"/": .}]')
@@ -32,7 +34,7 @@ while read -r line; do
     id: $id
   }')
 
-  echo "Sending request: $JSON"
+  #echo "Sending request: $JSON"
 
   curl -sS -X POST "$FOREST_RPC_URL" \
     -H "Content-Type: application/json" \
