b0e40748f08526ad9808a66602ee45d89573863c
Parth Shah <parthhshah171@gmail.com>
2025-07-22 21:24:40 +0000

    fix

--- workload/main/anytime-p2p-bomb.sh
+++ workload/main/anytime-p2p-bomb.sh
@@ -3,6 +3,6 @@
 echo "Starting anytime P2P bomb"
 
 # Run the P2P bomb
-/opt/antithesis/app -operation p2p-bomb -node "Lotus1"
+/opt/antithesis/app stress p2p-bomb -node "Lotus1"
 
 echo "P2P bomb completed"
