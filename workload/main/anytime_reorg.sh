9da2d976e012b2794c47e0a51792da723c7c3100
Parth Shah <parthhshah171@gmail.com>
2025-07-25 19:48:40 +0000

    fix shell script

--- workload/main/anytime_reorg.sh
+++ workload/main/anytime_reorg.sh
@@ -2,5 +2,20 @@
 set -e
 
 echo "Starting reorg simulation"
+# Array of available nodes
+NODES=("Lotus1" "Lotus2")
+
+# Function to get a random node
+get_random_node() {
+    local len=${#NODES[@]}
+    local rand_index=$((RANDOM % len))
+    echo "${NODES[$rand_index]}"
+}
+
+# Get a random node
+SELECTED_NODE=$(get_random_node)
+
+echo "Selected node for reorg simulation: $SELECTED_NODE"
+
 # Call the workload CLI reorg command
-/opt/antithesis/app network reorg --node "$NODE_NAME" --check-consensus
+/opt/antithesis/app network reorg --node "$SELECTED_NODE" --check-consensus
