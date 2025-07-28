692e8781709a9746f45b476fcaa8eea358317c4d
Parth Shah <parthhshah171@gmail.com>
2025-07-28 17:50:59 +0000

    remove wallet creation during startup

--- workload/main/first_check.sh
+++ workload/main/first_check.sh
@@ -5,7 +5,5 @@ set -e
 echo "Workload [main][first.sh]: initializing wallets..."
 
 python3 -u /opt/antithesis/resources/initialize_wallets.py "forest"
-/opt/antithesis/app wallet create --node Lotus1 --count 5
-/opt/antithesis/app wallet create --node Lotus2 --count 5
 
 echo "Workload [main][first.sh]: completed workload setup."
