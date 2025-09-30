4ad7fe1bb8fa9c9a2cd152457d20d2fe2d27fb9f
Parth Shah <parthhshah171@gmail.com>
2025-09-30 15:01:00 +0000

    refactor drivers, put assertions in eventually

--- workload/main/anytime_f3_status_health.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-echo "Running F3 status check for all nodes"
-
-/opt/antithesis/app monitor f3-status
--- workload/main/anytime_height_progression_health.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-echo "Running height progression monitoring for all nodes"
-
-/opt/antithesis/app monitor height-progression --duration 2m --interval 10s --max-stalls 15
