d72dd3e3433d33620901be6fdde17b1e89ea048e
Parth Shah <parthhshah171@gmail.com>
2026-02-06 08:32:49 +0000

    fixing migration issues

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-retries=10
-while [ "$retries" -gt 0 ]; do
-    echo "-------------------"
-    retries=$(( retries - 1 ))
-    echo "lotus${no}: $retries connection attempts remaining..."
-    echo "-------------------"
-done
